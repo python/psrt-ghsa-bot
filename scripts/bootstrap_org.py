f0c33b03edc30006a3ac85f9fa27b770e332a820
Jacob Coffee <jacob@z7x.org>
2025-11-19 15:20:06 -0600

    placeholder file

---
+++ scripts/bootstrap_org.py
@@ -0,0 +1,8 @@
+"""TODO: bootstrapping script for setting up a 'psrt' team if not existing.
+
+also other things tat might need to be done to get a test or ready for local dev
+with the bot
+
+also maybe we should make the psrt team thing configurable because that doesnt
+scale to other orgs that might want to use this bot
+"""
