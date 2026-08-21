68b965d8fd4dc40cbad9664d725db7f82edcb6b5
Seth Larson <sethmichaellarson@gmail.com>
2026-08-21 15:16:40 -0500

    Remove f-string

--- src/psrt_ghsa_bot/app.py
+++ src/psrt_ghsa_bot/app.py
@@ -263,7 +263,7 @@ def apply_to_repo(github: GitHub, owner: str, repo: str, cve_api: CveApi, *, res
         new_credits = get_security_advisory_credits(github, security_advisory)
         if new_credits and existing_credits != new_credits:
             patch_data["credits"] = new_credits
-            print(f"       📋 Will add credits for developing and reviewing remediation")
+            print("       📋 Will add credits for developing and reviewing remediation")
 
         # Apply updates, if any, to the security advisory.
         if patch_data:
